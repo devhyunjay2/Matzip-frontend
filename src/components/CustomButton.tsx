b32fce446b6c83a21ada7a5b50bf7e0b1985f2f3
hyunji <personal.hyunji@gmail.com>
2025-02-18 21:57:50 +0900

    Update: 공통컴포넌트 CustomButton 파일일 코드 순서 변경 및 스타일 수정정 -색상 -> 공통색상 정의값(colors.ts) 반영영

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -1,60 +1,92 @@
-import {Text, Pressable, StyleSheet, PressableProps} from 'react-native';
+import {
+  Text,
+  Pressable,
+  StyleSheet,
+  PressableProps,
+  Dimensions,
+  View,
+} from 'react-native';
 import React from 'react';
+import {colors} from '../constants/colors';
 
 interface CustomButtonProps extends PressableProps {
   label: string;
   variant?: 'filled' | 'outlined';
   size?: 'large' | 'medium';
+  inValid?: boolean;
 }
 
 export default function CustomButton({
   label,
   variant = 'filled',
   size = 'large',
+  // disable Props
+  inValid = false,
   ...props
 }: CustomButtonProps) {
   return (
     <Pressable
-      style={[styles.container, styles[variant], styles[size]]}
+      disabled={inValid}
+      style={({pressed}) => [
+        styles.container,
+        pressed ? styles[`${variant}Pressed`] : styles[variant],
+        inValid && styles.inValid,
+      ]}
       {...props}>
-      <Text style={[styles.text, styles[`${variant}Text`]]}>{label}</Text>
+      <View style={styles[size]}>
+        <Text style={[styles.text, styles[`${variant}Text`]]}>{label}</Text>
+      </View>
     </Pressable>
   );
 }
+const deviceHeight = Dimensions.get('screen').height;
 
 const styles = StyleSheet.create({
   container: {
     borderRadius: 3,
     justifyContent: 'center',
-  },
-
-  filled: {
-    backgroundColor: '#C63B64',
-  },
-  outlined: {
-    borderColor: '#C63B64',
-    borderWidth: 1,
-  },
-  large: {
-    width: '100%',
-    paddingVertical: 15,
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'row',
   },
   medium: {
     width: '50%',
-    paddingVertical: 12,
+    paddingVertical: deviceHeight > 700 ? 12 : 18,
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  large: {
+    width: '100%',
+    paddingVertical: deviceHeight > 700 ? 15 : 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
   },
   text: {
     fontSize: 16,
     fontWeight: '700',
   },
+  filled: {
+    backgroundColor: colors.PINK_700,
+  },
+  outlined: {
+    borderColor: colors.PINK_700,
+    borderWidth: 1,
+  },
+  filledPressed: {
+    backgroundColor: colors.PINK_500,
+  },
+  outlinedPressed: {
+    borderColor: colors.PINK_700,
+    borderWidth: 1,
+    opacity: 0.5,
+  },
   filledText: {
-    color: '#fff',
+    color: colors.WHITE,
   },
   outlinedText: {
-    color: '#C63B64',
+    color: colors.PINK_700,
+  },
+  inValid: {
+    opacity: 60,
   },
 });
