1a2e1ad929bceb405f76b4fffd8619ff7db5f967
hyunji <personal.hyunji@gmail.com>
2025-02-21 16:55:13 +0900

    Update: mergeRefs 함수적용용

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -5,10 +5,11 @@ import {
   Text,
   TextInputProps,
   View,
+  TextInput,
 } from 'react-native';
-import React, {useRef} from 'react';
-import {TextInput} from 'react-native-gesture-handler';
+import React, {ForwardedRef, forwardRef, useRef} from 'react';
 import {colors} from '../constants/colors';
+import {mergeRefs} from '../utils';
 
 interface InputFieldProps extends TextInputProps {
   disabled?: boolean;
@@ -16,41 +17,44 @@ interface InputFieldProps extends TextInputProps {
   touched?: boolean;
 }
 
-const InputField = ({
-  disabled = false,
-  error,
-  touched = false,
-  ...props
-}: InputFieldProps) => {
-  //1. 참조객체를 만듬 -TextInput 요소를 참조
-  const innerRef = useRef<TextInput | null>(null);
+const InputField = forwardRef(
+  (
+    {disabled = false, error, touched = false, ...props}: InputFieldProps,
+    ref?: ForwardedRef<TextInput | null>,
+  ) => {
+    //1. 참조객체를 만듬 -TextInput 요소를 참조
+    const innerRef = useRef<TextInput | null>(null);
 
-  // 2. focus()를 호출해서 강제로 입력창을 활성화
-  const handlePressInput = () => {
-    innerRef.current?.focus();
-  };
-  return (
-    <Pressable onPress={handlePressInput}>
-      <View style={[styles.constainer, disabled && styles.disabled]}>
-        <TextInput
-          editable={!disabled}
-          placeholderTextColor={colors.GRAY_500}
-          style={[
-            styles.input,
-            disabled && styles.disabled,
+    // 2. focus()를 호출해서 강제로 입력창을 활성화
+    const handlePressInput = () => {
+      innerRef.current?.focus();
+    };
+    return (
+      <Pressable onPress={handlePressInput}>
+        <View style={[styles.constainer, disabled && styles.disabled]}>
+          <TextInput
+            ref={ref ? mergeRefs(innerRef, ref) : innerRef}
+            editable={!disabled}
+            placeholderTextColor={colors.GRAY_500}
+            style={[
+              styles.input,
+              disabled && styles.disabled,
 
-            Boolean(error) && styles.inputError,
-          ]}
-          autoCapitalize="none"
-          spellCheck={false}
-          autoCorrect={false}
-          {...props}
-        />
-        {Boolean(error) && touched && <Text style={styles.error}>{error}</Text>}
-      </View>
-    </Pressable>
-  );
-};
+              Boolean(error) && styles.inputError,
+            ]}
+            autoCapitalize="none"
+            spellCheck={false}
+            autoCorrect={false}
+            {...props}
+          />
+          {Boolean(error) && touched && (
+            <Text style={styles.error}>{error}</Text>
+          )}
+        </View>
+      </Pressable>
+    );
+  },
+);
 
 export default InputField;
 
--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,33 +1,46 @@
-import {StyleSheet, View} from 'react-native';
-import React from 'react';
+import {StyleSheet, TextInput, View} from 'react-native';
+import React, {useRef} from 'react';
 import InputField from '../../components/InputField';
 import useForm from '../../hooks/useForm';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import CustomButton from '../../components/CustomButton';
 
+import {validateLogin} from '../../utils';
+
 const LoginScreen = () => {
-  const login = useForm({initialValue: {email: '', password: ''}});
+  const passwordRef = useRef<TextInput | null>(null);
+
+  const login = useForm({
+    initialValue: {email: '', password: ''},
+    validate: validateLogin,
+  });
 
   const handleSubmit = () => {
-    console.log('로그인완료');
+    console.log(login.values);
   };
   return (
-    <SafeAreaView>
-      <View style={styles.constainer}>
+    <SafeAreaView style={styles.constainer}>
+      <View style={styles.inputContainer}>
         <InputField
+          autoFocus
           placeholder="이메일"
-          error={'이메일을 입력하세요.'}
+          error={login.errors?.email}
           touched={login.touched.email}
           {...login.getTextInputProps('email')}
           inputMode="email"
+          blurOnSubmit={false}
+          onSubmitEditing={() => passwordRef.current?.focus()}
         />
         <InputField
+          ref={passwordRef}
           placeholder="비밀번호"
-          error={'비밀번호를 입력하세요.'}
+          error={login.errors?.password}
           touched={login.touched.password}
           {...login.getTextInputProps('password')}
           //  비밀번호마스킹 처리
           secureTextEntry
+          blurOnSubmit={false}
+          onSubmitEditing={handleSubmit}
         />
       </View>
       <CustomButton
