6c6c21d7854e77e670b4820c68c8dd3958f53112
hyunji <personal.hyunji@gmail.com>
2025-02-25 14:30:55 +0900

    Add: 회원가입 스크린 생성 및 로직 추가

--- src/screens/auth/SignupScreen.tsx
+++ src/screens/auth/SignupScreen.tsx
@@ -1,14 +1,75 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {StyleSheet, TextInput, View} from 'react-native';
+import React, {useRef} from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import InputField from '../../components/InputField';
+import useForm from '../../hooks/useForm';
+import {validateSignup} from '../../utils';
+import CustomButton from '../../components/CustomButton';
 
 const SignupScreen = () => {
+  const passwordRef = useRef<TextInput | null>(null);
+  const passwordConfirmRef = useRef<TextInput | null>(null);
+
+  const login = useForm({
+    initialValue: {email: '', password: '', passwordConfirm: ''},
+    validate: validateSignup,
+  });
+
+  const handleSubmit = () => {
+    console.log('회원가입 함수');
+  };
   return (
-    <View>
-      <Text>SignupScreen</Text>
-    </View>
+    <SafeAreaView style={styles.constainer}>
+      <View style={styles.inputContainer}>
+        <InputField
+          placeholder="이메일"
+          error={login.errors?.email}
+          touched={login.touched.email}
+          {...login.getTextInputProps('email')}
+          returnKeyType="next"
+          inputMode="email"
+          // 이메일 입력후 비밀번호로 이동
+          onSubmitEditing={() => passwordRef.current?.focus()}
+        />
+        <InputField
+          ref={passwordRef}
+          placeholder="비밀번호"
+          // 자동 비밀번호생성 방지지
+          textContentType="oneTimeCode"
+          error={login.errors?.password}
+          touched={login.touched.password}
+          {...login.getTextInputProps('password')}
+          returnKeyType="next"
+          //  비밀번호마스킹 처리
+          secureTextEntry
+          blurOnSubmit={false}
+          onSubmitEditing={() => passwordConfirmRef.current?.focus()}
+        />
+        <InputField
+          ref={passwordConfirmRef}
+          placeholder="비밀번호 확인"
+          error={login.errors?.passwordConfirm}
+          touched={login.touched.passwordConfirm}
+          {...login.getTextInputProps('passwordConfirm')}
+          returnKeyType="next"
+          //  비밀번호마스킹 처리
+          secureTextEntry
+          onSubmitEditing={handleSubmit}
+        />
+      </View>
+      <CustomButton label="회원가입" onPress={handleSubmit} />
+    </SafeAreaView>
   );
 };
 
-export default SignupScreen;
+const styles = StyleSheet.create({
+  constainer: {
+    flex: 1,
+    margin: 30,
+  },
+  inputContainer: {
+    gap: 20,
+  },
+});
 
-const styles = StyleSheet.create({});
+export default SignupScreen;
