68d9af3657b80ddafdfa0eca8f7e1b4a636b0b09
hyunji <personal.hyunji@gmail.com>
2025-02-26 17:28:39 +0900

    Update: App 을 QueryClientProvider 로 랩핑핑

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import AuthStackNavigations from './src/navigation/AuthStackNavigations';
+import {QueryClientProvider} from '@tanstack/react-query';
+import queryClient from './src/api/queryClient';
 
 function App() {
   return (
-    <NavigationContainer>
-      <AuthStackNavigations />
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <AuthStackNavigations />
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
 
