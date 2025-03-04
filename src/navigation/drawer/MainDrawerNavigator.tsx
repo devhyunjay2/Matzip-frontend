c4b643dc23742341f2363f01c9ef1c3eb5dac8f9
hyunji <personal.hyunji@gmail.com>
2025-03-04 18:05:43 +0900

    Add: 로그인한 유저 화면 파일 생성 -MainDrawerNavigator

---
+++ src/navigation/drawer/MainDrawerNavigator.tsx
@@ -0,0 +1,12 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+const MainDrawerNavigator = () => {
+  return (
+    <View>
+      <Text>MainDrawerNavigator</Text>
+    </View>
+  );
+};
+
+export default MainDrawerNavigator;
