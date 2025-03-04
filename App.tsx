745cc696006e996f8fad65c753e26c575b8574de
hyunji <personal.hyunji@gmail.com>
2025-03-04 20:07:20 +0900

    Update:  권한 분기 컴포넌트로 변경 반영

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import AuthStackNavigations from './src/navigation/AuthStackNavigations';
 import {QueryClientProvider} from '@tanstack/react-query';
 import queryClient from './src/api/queryClient';
+import RootNavigator from './src/navigation/root/RootNavigator';
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <NavigationContainer>
-        <AuthStackNavigations />
+        <RootNavigator />
       </NavigationContainer>
     </QueryClientProvider>
   );
