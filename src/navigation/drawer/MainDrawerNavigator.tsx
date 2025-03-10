56fdca15aa3bd0e6c1a1cf8540eb61e863a38c99
hyunji <personal.hyunji@gmail.com>
2025-03-10 23:03:28 +0900

    Update: 로그아웃  api 함수 추가가

--- src/navigation/drawer/MainDrawerNavigator.tsx
+++ src/navigation/drawer/MainDrawerNavigator.tsx
@@ -1,10 +1,20 @@
 import {Text, View} from 'react-native';
 import React from 'react';
+import useAuth from '../../hooks/queries/useAuth';
+import CustomButton from '../../components/CustomButton';
 
 const MainDrawerNavigator = () => {
+  const {logoutMutation} = useAuth();
+
   return (
     <View>
       <Text>MainDrawerNavigator</Text>
+      <CustomButton
+        onPress={() => {
+          logoutMutation.mutate();
+        }}
+        label={'로그아웃'}
+      />
     </View>
   );
 };
