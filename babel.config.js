d796a0cc6e4bdda45fc34dc6d629445276318783
hyunji <personal.hyunji@gmail.com>
2025-03-14 15:54:08 +0900

    Add: 절대경로로 변경 -alias 설정

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugin: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+  ],
 };
